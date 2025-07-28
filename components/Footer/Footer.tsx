61257fcd07fcb5ae300eeb0dbe76603f5f35baed
Andrew <122519034+DrewCoding@users.noreply.github.com>
2025-07-27 22:24:02 -0700

    Web files added

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./Footer.module.css";
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <p className={styles.copyright}>
+        &copy; {new Date().getFullYear()} Andrew Williams. All rights reserved.
+      </p>
+    </footer>
+  );
+}
